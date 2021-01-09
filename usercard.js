47443058f5fefe46a68b78600fb47cd5e941b303
Igor Napierala <igor.napierala@cognifide.com>
2021-01-09 20:52:11 +0100

    events

--- usercard.js
+++ usercard.js
@@ -41,10 +41,37 @@ class UserCard extends HTMLElement {
   constructor() {
     super();
 
+    this.showInfo = true;
+
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
     this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
     this.shadowRoot.querySelector("img").src = this.getAttribute("avatar");
+  }
+
+  toggleInfo() {
+    this.showInfo = !this.showInfo;
+
+    const info = this.shadowRoot.querySelector(".info");
+    const toggleBtn = this.shadowRoot.querySelector("#toggle-info");
+
+    if (this.showInfo) {
+      info.style.display = "block";
+      toggleBtn.innerText = "Hide Info";
+    } else {
+      info.style.display = "none";
+      toggleBtn.innerText = "Show Info";
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector("#toggle-info")
+      .addEventListener("click", () => this.toggleInfo());
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector("#toggle-info").removeEventListener();
   }
 }
 
